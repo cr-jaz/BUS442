e6ea08bc8597a68d2907e42b95280b60b4140e32
jcruz8 <jcruz8@ncsu.edu>
2025-05-05 19:26:26 -0400

    initial commit

---
+++ utils.py
@@ -0,0 +1,12 @@
+
+
+def addnum(a, b):
+	return a+b
+	
+if __name__ == "__main__":
+    # Every python file when executed has a special variable called __name__. If the file is executed
+    # directly, the value of the __name__ variable is equal to "__main__"
+    
+	print ("Executing the utils file")
+	result = addnum(5,3)
+	print(f"Result = {result}")
